054e6f7f996f28e04d24ef6e8332d13b97f8b3b7
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-19 15:29:07 -0700

    change the spotify and links animation to on click instead of mousein/out

--- assets/javascript/spotify.js
+++ assets/javascript/spotify.js
@@ -1,23 +1,45 @@
-$("#spotify").on("mouseenter", function() {
-  $("#spotify").animate({
-    "right": "0px",
-  });
+// event listener for spotify div button
+$("#spotifyImg").on("click", function() {
+  var spotify = $("#spotify");
+  if (spotify.attr("data-state") === "hidden") {
+    spotify.animate({
+      "right": "0px",
+    });
+    spotify.attr("data-state", "shown");
+  } else if (spotify.attr("data-state") === "shown") {
+    spotify.animate({
+      "right": "-300px",
+    });
+    spotify.attr("data-state", "hidden");
+  }
 })
 
-$("#spotify").on("mouseleave", function() {
-  $("#spotify").animate({
-    "right": "-300px",
-  })
+// change cursor when hover in/out of spotify logo
+$("#spotifyImg").hover(function() {
+  $(this).css("cursor", "pointer");
+}, function() {
+  $(this).css("cursor", "default");
 })
 
-$("#slideLinks").on("mouseenter", function() {
-  $("#slideLinks").animate({
-    "left": "0px",
-  });
+// event listener for links button
+$("#links").on("click", function() {
+  var slideLinks = $("#slideLinks");
+  if (slideLinks.attr("data-state") === "hidden") {
+    slideLinks.animate({
+      "left": "0px",
+    });
+    slideLinks.attr("data-state", "shown");
+  } else if (slideLinks.attr("data-state") === "shown") {
+    slideLinks.animate({
+      "left": "-138px",
+    });
+    slideLinks.attr("data-state", "hidden");
+  }
 })
 
-$("#slideLinks").on("mouseleave", function() {
-  $("#slideLinks").animate({
-    "left": "-138px",
-  })
+// change cursor when hover in/out of links
+$("#links").hover(function() {
+  $(this).css("cursor", "pointer");
+}, function() {
+  $(this).css("cursor", "default");
 })
